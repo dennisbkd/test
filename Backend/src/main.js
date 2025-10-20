0cd0d74644fbf8bd849df501697c6295fcc41fc0
dennisbkd <rasguidorondalesdennis@gmail.com>
2025-10-20 11:48:32 -0400

    Actualizar la URL de origen de CORS para apuntar al entorno de prueba

--- Backend/src/main.js
+++ Backend/src/main.js
@@ -28,7 +28,7 @@ export const App = ({
   app.use(express.json())
   app.use(express.urlencoded({ extended: true }))
   app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'http://test-d3f1.vercel.app',
     credentials: true
   }))
 
